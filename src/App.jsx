4e9ec4682d13109c484019ccf73469f3075fd105
DyakaFaiz <dyakafaiz123@gmail.com>
2024-03-02 20:59:10 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import Table from './assets/Table'
 import Footer from './assets/Footer'
-import Quotes from './assets/Quotes'
 
 function App() {
   return (
     <>
     <div className="wrapper">
-      <div className="content">
-        <div className="table">
           <Table />
-          <Quotes />
-          <Footer />
-        </div>
-      </div>
     </div>
     </>
   )
